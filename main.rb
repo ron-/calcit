94ffc61c70d7ae4db55a8f6db872f61156a41527
Ron Jones <rrrron@gmail.com>
2013-01-29 16:11:42 -0500

    Added calculator selection and quit

--- main.rb
+++ main.rb
@@ -1,3 +1,49 @@
+# Calculator selection and quit option -Ron
+
+puts "   _________    __   ________________"
+puts "  / ____/   |  / /  / ____/  _/_  __/"
+puts " / /   / /| | / /  / /    / /  / /   "
+puts "/ /___/ ___ |/ /__/ /____/ /  / /    "
+puts " \____/_/  |_/_____|____/___/ /_/     "
+
+puts "                                                           "
+puts "Welcome to CalcIT! The greatest calculator ever conceived by the human mind."
+puts "Please select a calculator:"
+print "Enter 'a' for advanced, 'b' for basic, or 'q' to quit calcit: "
+selection = gets.chomp.downcase
+
+while selection != "q"
+
+  if selection == "a"
+
+    #insert advanced calc
+    puts "advanced calc"
+
+
+  elsif selection == "b"
+
+    #insert basic calc
+    puts "basic calc"
+
+  else
+
+    puts "Please enter a valid response!"
+
+  end
+
+  print "Enter 'a' for advanced, 'b' for basic, or 'q' to quit calcit: "
+  selection = gets.chomp.downcase
+
+end
+
+#end of calculator selection and quit option -Ron
+
+
+
+
+
+
+
 def subtract(a, b)
   (a-b)
 end
